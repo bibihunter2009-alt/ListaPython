b1a9a1876a9411c3899cb44999be3a04d0f434d1
bibihunter2009-alt <bibihunter2009@gmail.com>
2026-05-04 07:51:53 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#--seletor de caracteres--
+letra = input("digite uma letra:").upper()
+letra == 'A':
+ print("voce selecionou a opçao A")
+elif letra == 'B':
+ print("voce selecionou a opçao B")
+else:
+ print("essa opçao nao esta disponivel")
