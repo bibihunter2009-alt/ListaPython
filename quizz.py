4208b1cfe408984ee719cb41c7cdb40cfd863dc2
bibihunter2009-alt <bibihunter2009@gmail.com>
2026-05-07 16:15:02 -0300

    Atualizar o quizz.py

--- quizz.py
+++ quizz.py
@@ -1,1 +1,39 @@
+#--quiz simples--
+print("Quando começou a Segunda Guerra Mundial?")
+print("1. 1939")
+print("2. 1945")
+print("3. 1914")
 
+resposta = input("Resposta: ")
+
+if resposta == "1":
+    print("Acertou")
+else:
+    print("Errou")
+
+
+print("Quem era o líder da Alemanha?")
+print("1. Hitler")
+print("2. Napoleon")
+print("3. Stalin")
+
+resposta = input("Resposta: ")
+
+if resposta == "1":
+    print("Acertou")
+else:
+    print("Errou")
+
+
+print("Quando terminou a Segunda Guerra Mundial?")
+print("1. 1939")
+print("2. 1945")
+print("3. 1918")
+
+resposta= input("Resposta: ")
+
+if resposta == "2":
+    print("Acertou")
+else:
+    print("Errou")
+#--gabriela Cristina --
